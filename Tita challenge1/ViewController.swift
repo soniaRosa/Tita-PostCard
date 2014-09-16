eb33799f96e4c3dae96469c5d44cb1351a2608cb
Joao Rosa <joaorosa@air-de-joao.lan>
2014-09-16 14:28:03 +0000

    first commit

--- Tita challenge1/ViewController.swift
+++ Tita challenge1/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //IBOutlet
+    @IBOutlet weak var titaTextField: UITextField!
+    @IBOutlet weak var titaLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var mybutton: UIButton!
+  
+    //IBAction
+    @IBAction func myButtonPressed(sender: AnyObject) {
+        nameLabel.hidden = false
+        nameLabel.text = titaTextField.text
+        titaTextField.text = ""
+        titaTextField.resignFirstResponder()
+        nameLabel.textColor = UIColor.blueColor()
+        mybutton.setTitle("Thanks for your comment", forState: UIControlState.Normal)
+       
+    }
+    
+    
                             
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
