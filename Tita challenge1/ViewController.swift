b885d86d272fb467b5324e59e85395ee3b6b954b
Joao Rosa <joaorosa@air-de-joao.lan>
2014-09-16 08:47:32 +0000

    Initial Commit

---
+++ Tita challenge1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tita challenge1
+//
+//  Created by Joao Rosa on 16/09/14.
+//  Copyright (c) 2014 Sonia Rosa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
